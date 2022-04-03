fa7e66f3c8aa7c2631d91eed2f9ff1a480fe96e5
Giancarlo Sierra <giancarlosierra2011@hotmail.com>
2022-04-03 14:54:36 -0500

    operator test working

--- index.js
+++ index.js
@@ -2,7 +2,28 @@
 exports.__esModule = true;
 var readline_sync_1 = require("readline-sync"); // imports question method
 function main() {
+    function isNumber(str) {
+        var Num = parseInt(str);
+        var isNum = !isNaN(Num);
+        return isNum;
+    }
+    function isOperator(operator) {
+        switch (operator) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                return true;
+            default:
+                return false;
+        }
+    }
     var firstString = (0, readline_sync_1.question)('Enter first numer:\n');
-    console.log(firstString);
+    var operator = (0, readline_sync_1.question)('Enter operator: \n');
+    var secondString = (0, readline_sync_1.question)('Enter second numer:\n');
+    var op = isOperator(operator);
+    console.log(op);
+    var firstNum = isNumber(firstString);
+    var secondNum = isNumber(secondString);
 }
 main();
--- index.ts
+++ index.ts
@@ -2,8 +2,36 @@ import { question } from "readline-sync";// imports question method
 
 function main(): void
 {
+function isNumber(str:string):boolean{
+    const Num= parseInt(str)
+    const isNum: boolean= !isNaN(Num)
+    return isNum
+}
+
+function isOperator(operator: string):boolean
+{
+    switch(operator)
+    {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true;
+            default:
+                return false;
+    }
+}
+
     const firstString: string= question('Enter first numer:\n')
-    console.log(firstString)
+    const operator: string= question('Enter operator: \n')
+    const secondString: string= question('Enter second numer:\n')
+    const op = isOperator(operator)
+
+    console.log(op)
+
+    const firstNum= isNumber(firstString)
+    const secondNum= isNumber(secondString)
+
 }
 
 main();
